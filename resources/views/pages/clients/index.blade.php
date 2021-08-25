fc22e4fc2735d7ed5fb9d37f9ac2bffebf3cbd6e
Mudassar Ali <mudsr3@gmail.com>
2021-08-25 12:47:35 +0500

    minor

--- resources/views/pages/clients/index.blade.php
+++ resources/views/pages/clients/index.blade.php
@@ -29,7 +29,7 @@
         <div class="card-body">
 
             @forelse ($clients as $client)
-                <table class="table table-responsive">
+                <table class="table table-responsive w-100 d-block d-md-table">
                     <thead>
                         <tr>
                             <th scope="col" class="text-muted">Addres</th>
