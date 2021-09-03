c0409d45e630687f4e23ea3fc7d7b591488db97f
Mudassar <mudsr3@gmail.com>
2021-09-04 02:44:56 +0500

    dahsboard design tweaks

--- app/Http/Livewire/Dashboard/Index.php
+++ app/Http/Livewire/Dashboard/Index.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $tasks;
+
+
+    public function mount()
+    {
+        $this->tasks = collect();
+    }
+
     public function render()
     {
         return view('livewire.dashboard.index')->extends('layouts.main');
--- resources/views/livewire/dashboard/index.blade.php
+++ resources/views/livewire/dashboard/index.blade.php
@@ -1,15 +1,15 @@
 <div>
    <div class="row">
-       <div class="col-md-6">
-        <div class="card card-custom">
+       <div class="col-md-5">
+        <div class="card card-custom card-stretch gutter-b">
             <div class="card-header flex-wrap border-0 pt-6 pb-0">
                 <div class="card-title text-center">
-                    <h3 class="card-label">Pending Tasks</h3>
+                    <h3 class="card-label text-muted">Pending Tasks</h3>
                 </div>
             </div>
 
             <div class="card-body">
-                {{-- @if ($tasks->count() > 0) --}}
+                @if ($tasks->count() > 0)
                     <table class="table table-responsive w-100 d-block d-md-table">
                         <thead>
                             <tr>
@@ -21,43 +21,81 @@
                         </thead>
                         <tbody>
 
+                            @foreach ($tasks as $task)
+
+                            @endforeach
+
                         </tbody>
                     </table>
-                {{-- @else
-                    <h3 class="text-muted text-center">No Renewal Exists</h3>
-                @endif --}}
+                @else
+                    <h3 class="text-muted text-center">No Pending Task Exists</h3>
+                @endif
             </div>
         </div>
        </div>
 
-       <div class="col-md-6">
-        <div class="card card-custom">
-            <div class="card-header flex-wrap border-0 pt-6 pb-0">
-                <div class="card-title text-center">
-                    <h3 class="card-label">Up coming Renewals</h3>
+       <div class="col-md-7">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="card card-custom card-stretch gutter-b">
+                       <div class="card-header flex-wrap border-0 pt-6 pb-0">
+                           <div class="card-title text-center">
+                               <h3 class="card-label text-primary">Up coming Renewals</h3>
+                           </div>
+                       </div>
+
+                       <div class="card-body">
+                           {{-- @if ($tasks->count() > 0) --}}
+                               <table class="table table-responsive w-100 d-block d-md-table">
+                                   <thead>
+                                       <tr>
+                                           <th scope="col" class="text-muted">Client No</th>
+                                           <th scope="col" class="text-muted">Contract No</th>
+                                           <th scope="col" class="text-muted">Document Type</th>
+                                           <th scope="col" class="text-muted">Due Date</th>
+                                       </tr>
+                                   </thead>
+                                   <tbody>
+
+                                   </tbody>
+                               </table>
+                           {{-- @else
+                               <h3 class="text-muted text-center">No Renewal Exists</h3>
+                           @endif --}}
+                       </div>
+                   </div>
+               </div>
+                <div class="col-md-12">
+                    <div class="card card-custom card-stretch gutter-b">
+                        <div class="card-header flex-wrap border-0 pt-6 pb-0">
+                            <div class="card-title text-center">
+                                <h3 class="card-label text-danger">Alerts</h3>
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            {{-- @if ($tasks->count() > 0) --}}
+                                <table class="table table-responsive w-100 d-block d-md-table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col" class="text-muted">Client No</th>
+                                            <th scope="col" class="text-muted">Contract No</th>
+                                            <th scope="col" class="text-muted">Document Type</th>
+                                            <th scope="col" class="text-muted">Due Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            {{-- @else
+                                <h3 class="text-muted text-center">No Renewal Exists</h3>
+                            @endif --}}
+                        </div>
+                    </div>
                 </div>
-            </div>
+           </div>
 
-            <div class="card-body">
-                {{-- @if ($tasks->count() > 0) --}}
-                    <table class="table table-responsive w-100 d-block d-md-table">
-                        <thead>
-                            <tr>
-                                <th scope="col" class="text-muted">Client No</th>
-                                <th scope="col" class="text-muted">Contract No</th>
-                                <th scope="col" class="text-muted">Document Type</th>
-                                <th scope="col" class="text-muted">Due Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
-                {{-- @else
-                    <h3 class="text-muted text-center">No Renewal Exists</h3>
-                @endif --}}
-            </div>
-        </div>
        </div>
    </div>
 </div>
